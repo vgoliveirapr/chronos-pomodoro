98a3393e0b60bbdc9388d0de9c6a58e0c9c21558
Victor Gabriel <gabvictoroli@gmail.com>
2025-08-27 13:14:30 -0300

    Css e Heading quase finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>Olá Mundo!</Heading>
       {/*Comentário é assim entre chaves dentro do componente
         Posso passar atributos também pelo componente escolhendo seu nome
         no exemplo tenho o attr que é um number
@@ -16,8 +16,11 @@ export function App() {
           Olá Mundo 2!
         </Heading>
       */}
-      <Heading attr={123} attr2='String'>
+      <Heading>
         Olá Mundo 2!
+        <button>
+          <TimerIcon />
+        </button>
       </Heading>
       <p>AAAAAAAAAAA</p>
     </>
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -5,10 +5,19 @@
 /* Importando um css modular - Exclusivo pode criar classes de css com o mesmo nome separar por modulos */
 import styles from './Heading.module.css';
 
-/*Usando Children do componente => Valor dentro das tags do Html
-  Nas dentro das tags <> são atributos
+/* Usando tipagem para os componentes nesse caso seria o props, garantindo que vai vir o valor correto
+   Da pra usar pra garantir que vai vir os valores pro componente
+   Tipo React.ReactNode => Todos os tipos que o react reconhece como children dele inclusive componentes
 */
-export function Heading(props) {
-  console.log(props.attr);
-  return <h1 className={styles.heading}>{props.children}</h1>;
+type HeadingProps = {
+  children: React.ReactNode;
+};
+
+/*Usando Children do componente => Valor dentro das tags do Html
+  Dentro das tags <> são atributos
+  Da pra desestruturar uma "classe" com { ABC } sendo ABC as "propriedades" da minha tipagem e
+  Sendo usado esse ABC como uma variável
+*/
+export function Heading({ children }: HeadingProps) {
+  return <h1 className={styles.heading}>{children}</h1>;
 }
