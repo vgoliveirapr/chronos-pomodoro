effa04db011b56a287017591b312a408d05d381b
Victor Gabriel <gabvictoroli@gmail.com>
2025-09-04 13:35:37 -0300

    Componentes criados e estilizados - Menu e CountDown, além de criação inicial do form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
 import { Logo } from './components/Logo';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 export function App() {
   return (
@@ -22,7 +23,32 @@ export function App() {
       </Container>
 
       <Container>
-        <Heading>Menu</Heading>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='input'>Task:</label>
+            <input id='input' type='text' />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Iniciar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
